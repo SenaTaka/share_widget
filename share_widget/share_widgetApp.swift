de5b4072a3d2b0bf9700387f6eb5af400a87fa33
SenaTaka <st0327t@gmail.com>
2026-03-27 14:27:09 +0900

    Initial Commit

---
+++ share_widget/share_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  share_widgetApp.swift
+//  share_widget
+//
+//  Created by Sena Takasawa on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct share_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
